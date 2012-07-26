069df389987baa4936582fb29fc51d2214120ec8
Ouning Zhang <ozhang@undertone.com>
2012-07-25 17:52:02 -0700

    another cotact

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,10 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def contact
+  end
+
+  def another_contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ SampleApp::Application.routes.draw do
   get "static_pages/help"
 
   get "static_pages/about"
+
+  get "static_pages/contact"
+
+  get "static_pages/another_contact"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
